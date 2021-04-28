e21cb6f97603fbc1db8a0ab16c0d64d800eb58fd
SerhiiMaletych <maitrewop@gmail.com>
2021-04-28 22:35:01 +0300

    10 - add CryptoConfig for password enctyption and SecurityConfig for manipulating with page access

---
+++ src/main/java/OnlineShop/OnlineShop/config/CryptoConfig.java
@@ -0,0 +1,15 @@
+package OnlineShop.OnlineShop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class CryptoConfig {
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+}
---
+++ src/main/java/OnlineShop/OnlineShop/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package OnlineShop.OnlineShop.config;
+
+import OnlineShop.OnlineShop.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/",
+                        "/search",
+                        "/registration",
+                        "/contacts",
+                        "/img/**",
+                        "/static/**",
+                        "/activate/*",
+                        "/product/*",
+                        "/menu/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and().csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(passwordEncoder);
+    }
+}
